f78bc84fc13db682314a7e7cfd8389b20e0371af
trevor-mill <trevor.million@chicagobooth.edu>
2022-01-30 20:57:47 +0000

    working through lecture

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,12 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
+  def play_rock
+    # code
+
+    # render({ :html => "<h1>Hello, world!</h1>".html_safe })
+
+    render({ :template => "game_templates/user_rock.html.erb"})
+  end
+
 end

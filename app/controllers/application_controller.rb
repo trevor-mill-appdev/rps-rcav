bc85857c550ebe6fa7819f5766ad12734bae6344
trevor-mill <trevor.million@chicagobooth.edu>
2022-01-30 21:35:25 +0000

    now we're cooking

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,13 +11,38 @@ class ApplicationController < ActionController::Base
 
 
   def play_rock
-
+    @comp_move = ["rock", "paper", "scissors"].sample
+    if @comp_move == "rock"
+      @outcome = "tied"
+    elsif @comp_move == "paper"
+      @outcome = "lost"
+    else
+      @outcome = "won"
+    end
     render({ :template => "game_templates/user_rock.html.erb"})
   end
 
   def play_paper
-
+    @comp_move = ["rock", "paper", "scissors"].sample
+    if @comp_move == "rock"
+      @outcome = "won"
+    elsif @comp_move == "paper"
+      @outcome = "tied"
+    else
+     @outcome = "lost"
+    end
     render({ :template => "game_templates/user_paper.html.erb"})
   end
 
+  def play_scissors
+    @comp_move = ["rock", "paper", "scissors"].sample
+    if @comp_move == "rock"
+      @outcome = "lost"
+    elsif @comp_move == "paper"
+      @outcome = "won"
+    elsif @comp_move == "scissors"
+      @outcome = "tied"
+    end
+    render{{ :template => "game_templates/user_scissors.html.erb"}}
+  end
 end
